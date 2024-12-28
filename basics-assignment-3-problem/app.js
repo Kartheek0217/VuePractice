5d4fc71b685632a6594bcba0649db892a093d3bb
Karthik Jakkula <50766097+Kartheek0217@users.noreply.github.com>
2024-12-28 21:11:04 +0530

    completed assignment-3

--- basics-assignment-3-problem/app.js
+++ basics-assignment-3-problem/app.js
@@ -4,9 +4,29 @@ const app = Vue.createApp({
       counterValue: 0,
     };
   },
+  computed: {
+    result() {
+      if (this.counterValue < 37) {
+        return "Not there yet";
+      } else if (this.counterValue === 37) {
+        return 37;
+      } else {
+        return "Too much!";
+      }
+    },
+  },
+  watch: {
+    result() {
+      const that = this;
+      setTimeout(function () {
+        that.counterValue = 0;
+      });
+    },
+  },
   methods: {
-    addCounter() {
-      this.counterValue = this.counterValue + 5;
+    addNumber(num) {
+      this.counterValue = this.counterValue + num;
+      console.log(this.counterValue);
     },
   },
 });
